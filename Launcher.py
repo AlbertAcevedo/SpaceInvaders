e787f0c0fa3bbf623742c566ebb794fd327a18da
Alberto Andres Acevedo Callejas <alan_ac07@hotmail.com>
2019-03-23 23:39:53 -0500

    added title and inst to keep screen going

---
+++ Launcher.py
@@ -0,0 +1,14 @@
+# Space Invaders for beginners
+
+
+import turtle
+import os
+
+# Set up screen
+wn = turtle.Screen()
+wn.bgcolor("darkblue")
+wn.title("Space invaders")
+
+
+# Keep window open until x is hit
+wn.mainloop()
