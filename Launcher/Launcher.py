3843133dc40f65c61d66c8b53b19eab7e878c706
Alberto Andres Acevedo Callejas <alan_ac07@hotmail.com>
2019-03-24 12:03:00 -0500

    added movement functions

--- Launcher/Launcher.py
+++ Launcher/Launcher.py
@@ -1,8 +1,28 @@
 # Space Invaders for beginners
-
-
 import turtle
 import os
+
+
+# @@@ All functions here @@@
+# Move the player
+def move_left():
+    x = player.xcor()
+    x -= p_speed
+    player.setx(x)
+
+
+def move_right():
+    x = player.xcor()
+    x += p_speed
+    player.setx(x)
+
+
+def move_up():
+    player.forward(p_speed)
+
+
+def move_down():
+    player.backward(p_speed)
 
 # Set up screen
 wn = turtle.Screen()
@@ -27,7 +47,7 @@ for side in range(4):
     bpen.lt(90)
 bpen.hideturtle()
 
-#Create the player
+# Create the player
 player = turtle.Turtle()
 player.color("green")
 player.shape("triangle")
@@ -35,6 +55,13 @@ player.penup()
 player.speed(0)
 player.setposition(0, -250)
 player.setheading(90)
+p_speed = 15
 
+# Create keyboard bindings
+wn.onkey(move_left, "Left")
+wn.onkey(move_right, "Right")
+wn.onkey(move_up, "Up")
+wn.onkey(move_down, "Down")
+wn.listen()
 # Keep window open until x is hit
 wn.mainloop()
